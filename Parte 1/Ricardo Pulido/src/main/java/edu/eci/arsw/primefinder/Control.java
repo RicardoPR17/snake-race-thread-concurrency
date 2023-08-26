8946a5512e3259b113bc7c094c06bfaea8d6fd07
RicardoPR17 <rpulidor17.rpr18@gmail.com>
2023-08-26 09:47:36 -0500

    Formato y gitignore

--- Parte 1/Ricardo Pulido/src/main/java/edu/eci/arsw/primefinder/Control.java
+++ Parte 1/Ricardo Pulido/src/main/java/edu/eci/arsw/primefinder/Control.java
@@ -9,7 +9,7 @@ package edu.eci.arsw.primefinder;
  *
  */
 public class Control extends Thread {
-    
+
     private final static int NTHREADS = 3;
     private final static int MAXVALUE = 30000000;
     private final static int TMILISECONDS = 5000;
@@ -17,28 +17,28 @@ public class Control extends Thread {
     private final int NDATA = MAXVALUE / NTHREADS;
 
     private PrimeFinderThread pft[];
-    
+
     private Control() {
         super();
-        this.pft = new  PrimeFinderThread[NTHREADS];
+        this.pft = new PrimeFinderThread[NTHREADS];
 
         int i;
-        for(i = 0;i < NTHREADS - 1; i++) {
-            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA);
+        for (i = 0; i < NTHREADS - 1; i++) {
+            PrimeFinderThread elem = new PrimeFinderThread(i * NDATA, (i + 1) * NDATA);
             pft[i] = elem;
         }
-        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1);
+        pft[i] = new PrimeFinderThread(i * NDATA, MAXVALUE + 1);
     }
-    
+
     public static Control newControl() {
         return new Control();
     }
 
     @Override
     public void run() {
-        for(int i = 0;i < NTHREADS;i++ ) {
+        for (int i = 0; i < NTHREADS; i++) {
             pft[i].start();
         }
     }
-    
+
 }
